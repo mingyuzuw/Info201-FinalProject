c3d53e9ce87253b4a0dbbcc949e78e80a58089a3
Mingyu Zhong <mingyuz@uw.edu>
2019-06-03 23:36:43 -0700

    halfway done with Estimation Part

--- server.R
+++ server.R
@@ -4,11 +4,53 @@ library(ggplot2)
 library(R.utils)
 
 AP_df <- data.table::fread("Admission_Predict_Ver1.1.csv", stringsAsFactors = FALSE)
-
+univ_level <- c("Very Competitive", "Competitive", "Average", "Easy", "Very Easy")
 shinyServer(function(input, output) {
+  output$introduction <- renderText({
+    paste0("This ...")
+  })
   # James's Part 
   get_df <- reactive({
-    
+    univ_df <- AP_df[AP_df$University_Rating == input$univ, ]
+    return(univ_df)
+  })
+  output$instruction <- renderText({
+    paste0()
+  })
+  output$difficulty <- renderText({
+    paste0("First, select the appropriate university rating to start.")
+  })
+  output$Est_Des <- renderText({
+    univ_df <- get_df()
+    coe <- lm(Chance_of_Admit ~ TOEFL_Score + GRE_Score + CGPA + Research,
+            data = univ_df)
+    coe <- coe$coefficients
+    coe <- as.numeric(coe)
+    ts <- coe[2] * input$TS
+    gs <- coe[3] * input$GS
+    gpa <- coe[4] * input$GPA
+    re <- coe[5] * input$research
+    # est_AR <- coe[1]
+    print(typeof(coe[1]))
+    # paste0("The estimated admission rate is for the ", univ_level[input$univ], "university,
+    #        it is based on the given user input data which is shown in the table below. The plot
+    #        below shows the every data point in the dataset and the generated linear regression line. 
+    #        The estimation for the current user is ", est_AR, "%.")
+    paste0(re)
+  })
+  output$Factors_v_AR <- renderPlot({
+    univ_df <- get_df()
+    current_graph <- ggplot(univ_df,aes(x = TOEFL_Score + GRE_Score + 
+                                               CGPA + Research,
+                                             y = Chance_of_Admit)) + 
+      geom_point() + 
+      geom_smooth(method = "lm", se = FALSE) + ggtitle(univ_level[input$univ])
+    print(current_graph)
+  })
+  output$User_data <- renderTable({
+    user_input <- list("TOEFL Score" = input$TS, "GRE Score" = input$GS,
+                       "CPGA" = input$GPA, 
+                       "Research Experience" = ifelse(1, "Yes", "No"))
   })
   
   # Sean's Part
--- ui.R
+++ ui.R
@@ -4,53 +4,52 @@ shinyUI(fluidPage(
   tabsetPanel(
     # Overview/Introduction Page
     tabPanel("Overivew", fluid = TRUE,
-             sidebarLayout(
-               
+             fluidRow(
+               textOutput("introduction")
              )
     ),
     # James's Part
     tabPanel("Estimation", fluid = TRUE,
+             fluidRow(
+               textOutput("instruction")
+             ),
              sidebarLayout(
                sidebarPanel(
                  textOutput("difficulty"),
-                 radioButtons("univ", label = h3("University Rating"),
+                 radioButtons("univ", 
+                              label = h3("University Rating(Level of difficulty)"),
                               choices = list("Very Competitive" = 1, 
-                                             "competitive" = 2,
-                                             "Normal/Average" = 3,
+                                             "Competitive" = 2,
+                                             "Average" = 3,
                                              "Easy" = 4, 
                                              "Very Easy" = 5), 
                               selected = 3),
-                 sliderInput("T_Score",
-                             "TOEFL Score",
-                             min = 0,
-                             max = 120,
-                             value = 100),
-                 sliderInput("G_Score",
-                             "GRE Score",
-                             min = 0,
-                             max = 340,
-                             value = 300),
+                 numericInput("TS", label = h3("TOEFL Score"), value = 100),
+                 numericInput("GS", label = h3("GRE Score"), value = 300),
+                 numericInput("GPA", label = h3("CGPA"), value = 8),
                  radioButtons("research", label = h3("Research Experience"),
-                              choices = list("Yes" = 1, "No" = 2), 
-                              selected = 2)
+                              choices = list("Yes" = 1, "No" = 0), 
+                              selected = 1)
                ),
                
                mainPanel(
-                 plotOutput(""),
-                 textOutput("")
+                 textOutput("Est_Des"),
+                 tableOutput("User_data"),
+                 plotOutput("Factors_v_AR")
                )
                )
              )
-    ),
-  
+    )
+    ,
+
     # Sean's Part
     tabPanel("Report", fluid = TRUE,
              sidebarLayout(
                sidebarPanel(
-                 
+
                ),
                mainPanel(fluidRow(
-                 
+
                )
                )
              )
